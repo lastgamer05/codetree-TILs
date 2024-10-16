afac7cae754166142d2652a95500e036b40db1f0
lastgamer05 <105638752+lastgamer05@users.noreply.github.com>
2024-10-17 00:53:30 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241017/데이터 교환/exchange-data.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5, b = 6, c = 7;
+    int temp = 0;
+    temp = b;
+    b = a;
+    a = c;
+    c = temp;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c;
+    
+    return 0;
+}
